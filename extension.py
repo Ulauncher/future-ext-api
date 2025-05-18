68450ee7d96605ecb0383cc66517795491949479
Oleksandr Gornostal <al.gornostal@gmail.com>
2025-05-18 08:02:01 +0100

    more updates

--- extension.py
+++ extension.py
@@ -18,7 +18,7 @@ To iterate over the API design, making it
 - future-proof (i.e. tile view for images, etc.)
 """
 
-from ulauncher.api import Extension, Result
+from ulauncher.api import Extension, RowResult
 from bravesearch.queries import BraveQueries
 from bravesearch.errors import SearchError
 
@@ -32,40 +32,54 @@ class BraveExtension(Extension):
     - When the image search keyword is used, the results are displayed as tiles with images and 2 lines of text below (domain name + snippet)
     """
 
+    def on_launch(self):
+        # Show user a possibility to search all content, only images, only videos, only discussions:
+        self.set_query_modifiers(
+            {
+                "all": "All",
+                "images": "Images",
+                "videos": "Videos",
+                "discussions": "Discussions",
+            }
+        )
+
     def on_input(self, input_text: str, trigger_id: str):
+        # Validate the API keys
         if (
             not self.preferences["search_api_key"]
             or not self.preferences["autosuggest_api_key"]
         ):
             return [
-                Result(
+                RowResult(
                     name="Error",
                     description="Please set your API keys in the preferences.",
                     keep_app_open=True,
                 )
             ]
 
+        # Prompt the user to enter a search query
         bs = BraveQueries(
             self.preferences["search_api_key"], self.preferences["autosuggest_api_key"]
         )
         if not input_text.strip():
             return [
-                Result(
+                RowResult(
                     name="Brave Search",
                     description="Type your search query...",
                     keep_app_open=True,
                 )
             ]
 
+        # Show search suggestions as the user types
         try:
             return [
-                Result(compact=True, name=s) for s in bs.search_suggestions(input_text)
+                RowResult(compact=True, name=s, on_enter=None) for s in bs.search_suggestions(input_text)
             ]
         except SearchError as e:
-            return [e.to_result_item()]
+            return [e.to_row_result()]
         except Exception:
             return [
-                Result(
+                RowResult(
                     name="Error",
                     description="An error occurred while fetching suggestions.",
                     keep_app_open=True,
